80c24ae265041825f3e8d6d8e101e4bef3fc7961
Teffyhelo <167527825+Teffyhelo@users.noreply.github.com>
2025-08-04 12:24:18 -0300

    respostas

--- sript.js
+++ sript.js
@@ -3,3 +3,10 @@ const caixaPerguntas = document.querySelector('.caixa.perguntas');
 const caixaAlternativas = document.querySelector('.caixa.alternativa');
 const caixaResultado = document.querySelector('.caixa.resultado');
 const textoResultado = document.querySelector('.texto.resultado');
+
+const perguntas=[ 
+    {
+        enunciado: O que é futvolei,
+        alternativas:[
+{texto: 
+
